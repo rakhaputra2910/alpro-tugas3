08778922443ee91d6db47fda2ec930bef144a900
rakhaputra2910 <115854656+rakhaputra2910@users.noreply.github.com>
2022-12-14 19:42:24 +0700

    Add files via upload

---
+++ tugasalpro3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class tugasalpro3 {
+	      public static void main(String[] args) {
+	      Scanner data = new Scanner(System.in);
+	      System.out.print("Masukkan banyak baris : ");
+
+	      int nilai = data.nextInt();
+	      int j,i;
+	      for (i=1;i<=nilai ;i++ )
+	     {
+	           for (j=1;j<=i ;j++ )
+	     {
+	           System.out.print("*");
+	     }
+	          System.out.println();
+	  }
+	    }
+	}
